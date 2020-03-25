986e69c45440d49908df0f274593141d5d65fcfc
lisianthus <lisianthus623@gmail.com>
2020-03-25 21:15:08 +0700

    update api-handing

--- corona/services.py
+++ corona/services.py
@@ -12,29 +12,70 @@ import requests
 data_by_country = requests.get('https://corona.lmao.ninja/countries/')
 data_all = requests.get('https://corona.lmao.ninja/all')
 
-def service_api(request):
-    # varaibles
-    datas_country = []
+
+def main_api(request):
     datas_all = []
-    paginate_by = 20
-
-    # loop data by country
-    for i in data_by_country.json():
-        datas_country.append(i['country'])
-    # loop data all
+    total_data = countries(request=request)
     for i, j in data_all.json().items():
-        datas_all.append(j)
+        datas_all.append({str.upper(i): j})
 
+    # context
+    context = {
+        "total_data": len(total_data),
+        "datas": datas_all[:3],
+        "last_update": last_update()
+    }
+    return context
+
+
+def card_api(request):
+    total_data = countries(request=request)
 
     # paginate
+    paginate_by = 20
     paginator = Paginator(data_by_country.json(), paginate_by)
     page = request.GET.get('page')
     datas = paginator.get_page(page)
 
     # context
     context = {
-        "total_data": len(datas_country),
-        "data_all": datas_all,
-        "datas_country": datas
+        "total_data": len(total_data),
+        "last_update": last_update(),
+        "datas": datas,
     }
     return context
+
+
+def countries(request):
+    # variables
+    datas_country = []
+    for i in data_by_country.json():
+        datas_country.append(i['country'])
+
+    return datas_country
+
+
+def last_update():
+    datas_all = []
+    for i, j in data_all.json().items():
+        datas_all.append({str.upper(i): j})
+
+    update_time = int(datas_all[-1]['UPDATED'])
+    now = int(round(time.time() * 1000))
+    last_update = now - update_time
+
+    sec, a = divmod(last_update, 1000)
+    minute, b = divmod(sec, 60)
+    hour, c = divmod(minute, 60)
+    day, d = divmod(hour, 24)
+    week, e = divmod(day, 7)
+    if minute == 0:
+        return f"{sec} seconds"
+    elif hour == 0:
+        return f"{minute} minute"
+    elif day == 0:
+        return f"{hour} hour"
+    elif week == 0:
+        return f"{day} day"
+    else:
+        return f"{week} week"

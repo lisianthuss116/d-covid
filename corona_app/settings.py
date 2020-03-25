2eaad71957c22258b6c6891babc599a361892277
lisianthus <lisianthus623@gmail.com>
2020-03-25 11:16:36 +0700

    update app

--- corona_app/settings.py
+++ corona_app/settings.py
@@ -114,4 +114,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

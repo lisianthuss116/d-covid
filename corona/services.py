84fa6f6e80b4cd0cd8ce7eab02363f8eb1a7dc0e
lisianthus <lisianthus623@gmail.com>
2020-03-26 19:17:51 +0700

    adding page handler

--- corona/services.py
+++ corona/services.py
@@ -1,85 +1,81 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-import datetime
 import time
 import json
 import requests
 
 
-class ApiService:
-
-    def main_api(request):
-        datas_all = []
-        data_all = requests.get('https://corona.lmao.ninja/all')
-        total_data = ApiService.countries(request=request)
-        for i, j in data_all.json().items():
-            datas_all.append({str.upper(i): j})
-
-        # context
-        context = {
-            "total_data": len(total_data),
-            "datas": datas_all[:3],
-            "last_update": ApiService.last_update()
-        }
-        return context
+# endpoint
+data_all = requests.get('https://corona.lmao.ninja/all')
+data_by_country = requests.get('https://corona.lmao.ninja/countries/')
 
 
-    def card_api(request, limit=None):
-        data_by_country = requests.get('https://corona.lmao.ninja/countries/')
-        total_data = ApiService.countries(request=request)
+def main_api(request):
+    datas_all = []
+    total_data = countries(request=request)
+    for i, j in data_all.json().items():
+        datas_all.append({str.upper(i): j})
 
-        # paginate
-        if limit is not None:
-            paginate_by = limit
-        else:
-            paginate_by = 20
-        paginator = Paginator(data_by_country.json(), paginate_by)
-        page = request.GET.get('page')
-        datas = paginator.get_page(page)
-
-        # context
-        context = {
-            "total_data": len(total_data),
-            "last_update": ApiService.last_update(),
-            "datas": datas,
-        }
-        return context
+    # context
+    context = {
+        "total_data": len(total_data),
+        "datas": datas_all[:3],
+        "last_update": last_update()
+    }
+    return context
 
 
-    def countries(request):
-        data_by_country = requests.get('https://corona.lmao.ninja/countries/')
-        # variables
-        datas_country = []
-        for i in data_by_country.json():
-            datas_country.append(i['country'])
+def card_api(request, limit=None):
+    total_data = countries(request=request)
 
-        return datas_country
+    # paginate
+    if limit is not None:
+        paginate_by = limit
+    else:
+        paginate_by = 20
+    paginator = Paginator(data_by_country.json(), paginate_by)
+    page = request.GET.get('page')
+    datas = paginator.get_page(page)
+
+    # context
+    context = {
+        "total_data": len(total_data),
+        "last_update": last_update(),
+        "datas": datas,
+    }
+    return context
 
 
-    def last_update():
-        data_all = requests.get('https://corona.lmao.ninja/all')
-        datas_all = []
-        for i, j in data_all.json().items():
-            datas_all.append({str.upper(i): j})
+def countries(request):
+    # variables
+    datas_country = []
+    for i in data_by_country.json():
+        datas_country.append(i['country'])
 
-        update_time = int(datas_all[-1]['UPDATED'])
-        now = int(round(time.time() * 1000))
-        last_update = now - update_time
+    return datas_country
 
-        sec, a = divmod(last_update, 1000)
-        minute, b = divmod(sec, 60)
-        hour, c = divmod(minute, 60)
-        day, d = divmod(hour, 24)
-        week, e = divmod(day, 7)
-        if minute == 0:
-            return f"{sec} seconds"
-        elif hour == 0:
-            return f"{minute} minute"
-        elif day == 0:
-            return f"{hour} hour"
-        elif week == 0:
-            return f"{day} day"
-        else:
-            return f"{week} week"
+
+def last_update():
+    datas_all = []
+    for i, j in data_all.json().items():
+        datas_all.append({str.upper(i): j})
+
+    update_time = int(datas_all[-1]['UPDATED'])
+    now = int(round(time.time() * 1000))
+    last_update = now - update_time
+
+    sec, a = divmod(last_update, 1000)
+    minute, b = divmod(sec, 60)
+    hour, c = divmod(minute, 60)
+    day, d = divmod(hour, 24)
+    week, e = divmod(day, 7)
+    if minute == 0:
+        return f"{sec} seconds"
+    elif hour == 0:
+        return f"{minute} minute"
+    elif day == 0:
+        return f"{hour} hour"
+    elif week == 0:
+        return f"{day} day"
+    else:
+        return f"{week} week"
--- corona/views.py
+++ corona/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
-from .services import ApiService
+from .services import *
 
 
 def main(request):
-    context = ApiService.main_api(request=request)
-    return render(request, 'index.html', context)
+    try:
+        context = main_api(request=request)
+        return render(request, 'index.html', context)
+    except:
+        return render(request, 'handler/internal_err.html')
 
 
 def as_card(request):
-    limit = request.GET.get('limit')
-    context = ApiService.card_api(request=request, limit=limit)
-    return render(request, 'as_card.html', context)
+    try:
+        limit = request.GET.get('limit')
+        context = card_api(request=request, limit=limit)
+        return render(request, 'as_card.html', context)
+    except:
+        return render(request, 'handler/internal_err.html')
 
 
 def detailed(request):
-    context = ApiService.detailed(request=request)
+    context = detailed(request=request)
     return render(request, 'detailed.html', context)

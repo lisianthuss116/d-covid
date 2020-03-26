a86b08076db4def9c778f2b7845b9d2e3aa5a021
lisianthus <lisianthus623@gmail.com>
2020-03-26 18:24:12 +0700

    changes and updates

--- corona/views.py
+++ corona/views.py
@@ -1,26 +1,18 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
-import datetime
-import time
-import json
-import requests
-
-from .services import *
+from .services import ApiService
 
 
 def main(request):
-    context = main_api(request=request)
+    context = ApiService.main_api(request=request)
     return render(request, 'index.html', context)
 
 
 def as_card(request):
     limit = request.GET.get('limit')
-    context = card_api(request=request, limit=limit)
+    context = ApiService.card_api(request=request, limit=limit)
     return render(request, 'as_card.html', context)
 
 
 def detailed(request):
-    context = detailed(request=request)
+    context = ApiService.detailed(request=request)
     return render(request, 'detailed.html', context)

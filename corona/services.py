b09d713c301ebed0b11f73790903120951e07199
lisianthus <lisianthus623@gmail.com>
2020-03-31 22:59:17 +0700

    refactor(services.py): simplify & cleanup code

--- corona/services.py
+++ corona/services.py
@@ -2,6 +2,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 import time
 import json
+import datetime
 import requests
 
 
@@ -11,17 +12,17 @@ data_by_country = requests.get('https://corona.lmao.ninja/countries/')
 
 
 def main_api(request):
+    fucking_list = ['updated', 'affectedcountries']
+
     datas_all = []
-    total_data = countries(request=request)
     for i, j in data_all.json().items():
-        if str.lower(i) == 'updated':
+        if str.lower(i) in fucking_list:
             pass
         else:
-            datas_all.append({i: j})
+            datas_all.append({str.upper(i): j})
 
     # context
     context = {
-        "total_data": len(total_data),
         "datas": datas_all,
         "last_update": last_update()
     }
@@ -29,56 +30,23 @@ def main_api(request):
 
 
 def card_api(request, limit=None):
-    total_data = countries(request=request)
-
     # paginate
-    if limit is not None:
-        paginate_by = limit
-    else:
-        paginate_by = 20
+    paginate_by = limit if limit is not None else 20
     paginator = Paginator(data_by_country.json(), paginate_by)
     page = request.GET.get('page')
     datas = paginator.get_page(page)
 
     # context
     context = {
-        "total_data": len(total_data),
+        "total_data": data_all.json()['affectedCountries'],
         "last_update": last_update(),
         "datas": datas,
     }
     return context
 
 
-def countries(request):
-    # variables
-    datas_country = []
-    for i in data_by_country.json():
-        datas_country.append(i['country'])
-
-    return datas_country
-
-
 def last_update():
-    datas_all = []
-    for i, j in data_all.json().items():
-        datas_all.append({i: j})
-
-    update_time = int(datas_all[-2]['updated'])
-    now = int(round(time.time() * 1000))
-    last_update = now - update_time
-
-    sec, a = divmod(last_update, 1000)
-    minute, b = divmod(sec, 60)
-    hour, c = divmod(minute, 60)
-    day, d = divmod(hour, 24)
-    week, e = divmod(day, 7)
-    if minute == 0:
-        return f"{sec} seconds"
-    elif hour == 0:
-        return f"{minute} minute"
-    elif day == 0:
-        return f"{hour} hour"
-    elif week == 0:
-        return f"{day} day"
-    else:
-        return f"{week} week"
+    unix_timestamp = data_all.json()['updated']
+    last_update = datetime.datetime.fromtimestamp(
+        unix_timestamp//1000.0).strftime('%A, %B %d %Y, %H:%M:%S %p (UTC)')
+    return last_update
